64c5c583fdc42276e7ee52538c2c8d4f18b9e2c0
Georgeilsa <73441940+Georgeilsa@users.noreply.github.com>
2020-12-13 12:13:55 +0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "BitString.h"
+#include "Test.h"
+#include "List.h"
+
+
+
+
+int main()
+{
+
+
+
+	Test_int_list();
+	Test_float_list();
+	Test_BitString_list();
+}
+
+
+	 
+	
+
+
+
+
+
+
+
+
+
+	
+
